143d392fa12928d1e856bb3e63c709a50ca0590d
Piagrammist <Piagrammist@gmail.com>
2024-04-13 18:47:35 +0330

    Improvements on `/clear`

--- src/commands/chat/clear.ts
+++ src/commands/chat/clear.ts
@@ -14,21 +14,34 @@ export default {
     async execute(interaction: ChatInputCommandInteraction) {
         let count = interaction.options.getInteger('count')
         await interaction.reply(`Deleting ${count ?? 'all'} messages...`)
-        const breakpoint = (parseInt((await interaction.fetchReply()).id) - 1).toString()
+        const breakpoint = (await interaction.fetchReply()).id
         count ??= 100
 
+        let result
         let deleted = 0
         const channel = interaction.channel as TextChannel
         while (true) {
-            const result = await channel.bulkDelete(
-                await channel.messages.fetch({ limit: count, before: breakpoint, cache: false })
-            )
-            deleted += result.size
-            if (result.size < 100) {
-                break
+            try {
+                result = await channel.bulkDelete(
+                    await channel.messages.fetch({
+                        limit: count,
+                        before: breakpoint,
+                    })
+                )
+            } catch (error) {
+                console.error(error)
+                await interaction.editReply(
+                    'There was an error while executing this command!'
+                )
             }
+            deleted += result?.size ?? 0
+            if ((result?.size ?? 0) < 100) break
         }
 
-        await interaction.editReply(`Successfully deleted ${deleted} messages.`)
+        await interaction.editReply(
+            deleted > 0
+                ? `Successfully deleted ${deleted} messages.`
+                : 'No message found to delete!'
+        )
     },
 }
